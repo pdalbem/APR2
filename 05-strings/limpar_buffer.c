0d305036879d0f3149b211511ec8f4bd3c5e4e6e
pablo <dalbem@gmail.com>
2026-08-25 13:57:55 -0300

    initial commit

--- 05-strings/limpar_buffer.c
+++ 05-strings/limpar_buffer.c
@@ -3,12 +3,21 @@
 
 int main()
 {
+    int idade;
+    printf("Digite sua idade: ");
+    scanf("%d", &idade); //deixou \n no buffer
+
+    while (getchar()!='\n');
+    
     char nome[30];
     printf("Digite seu nome: ");
-    fgets(nome, sizeof(nome), stdin);  //usuário pode digitar mais que 29 chars. O fgets captura somente 29 chars, deixando resto no buffer
-    nome[strcspn(nome, "\n")] = '\0'; // remove \n do final
-    
-    while (getchar() != '\n' && getchar()!= EOF); //limpeza de buffer
+    fgets(nome, sizeof(nome), stdin); // usuário pode digitar mais que 29 chars. O fgets captura somente 29 chars, deixando resto no buffer
+
+    //procura \n para saber se houve buffer overflow
+    if (strchr(nome, '\n') == NULL)
+        while (getchar() != '\n'); // \n não encontrado, buffer overflow
+    else
+        nome[strcspn(nome, "\n")] = '\0'; // \n encontrado (não ocorreu buffer overflow) e substituído
 
     char curso[20];
     printf("Digite seu curso: ");
